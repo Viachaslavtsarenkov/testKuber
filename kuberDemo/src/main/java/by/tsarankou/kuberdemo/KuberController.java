105f50c3cf7ec2cf423303dcc10e21acfbbe7188
tsaren99 <tsaren99@sam-solutions.net>
2024-01-29 13:28:14 +0300

    add getting host name

--- kuberDemo/src/main/java/by/tsarankou/kuberdemo/KuberController.java
+++ kuberDemo/src/main/java/by/tsarankou/kuberdemo/KuberController.java
@@ -3,11 +3,14 @@ package by.tsarankou.kuberdemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class KuberController {
 
     @GetMapping("/")
-    public String getData() {
-        return "Data from server";
+    public String getData() throws UnknownHostException {
+        return "Host name " + InetAddress.getLocalHost();
     }
 }
